22a0612a6ab49099e2ef5dd5f3f85c63f558c0a0
Roynn <55062240+Royynn@users.noreply.github.com>
2025-09-11 16:18:49 +0300

    Update nhub.lua

--- nhub.lua
+++ nhub.lua
@@ -1,23 +1,186 @@
+local Players = game:GetService("Players")
+local TeleportService = game:GetService("TeleportService")
+PlaceId, JobId = game.PlaceId, game.JobId
+
+cc = game.Players.LocalPlayer.OnTeleport:Connect(function(State)
+    queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/Royynn/nhub/refs/heads/main/nhub.lua"))()')
+end)
+
 local Luna = loadstring(game:HttpGet("https://raw.githubusercontent.com/Nebula-Softworks/Luna-Interface-Suite/refs/heads/master/source.lua", true))()
 local Window = Luna:CreateWindow({
 	Name = "nigger hub",
 	LogoID = "82795327169782",
 	LoadingEnabled = false
 })
+-- main
 local MainTab = Window:CreateTab({
-	Name = "home",
+	Name = "Home",
+	Icon = "house",
+	ImageSource = "Material",
+	ShowTitle = true
+})
+local m1 = MainTab:CreateButton({
+	Name = "rejoin",
+	Description = nil,
+    	Callback = function()
+    	    if #Players:GetPlayers() <= 1 then
+                Players.LocalPlayer:Kick("\nRejoining...")
+                wait()
+                TeleportService:Teleport(PlaceId, Players.LocalPlayer)
+            else
+                TeleportService:TeleportToPlaceInstance(PlaceId, JobId, Players.LocalPlayer)
+            end
+    	end
+})
+local m3 = MainTab:CreateButton({
+	Name = "destroy gui",
+	Description = nil,
+    	Callback = function()
+    	    cc:Disconnect()
+    	    cc = nil
+            Luna:Destroy()
+            script:Destroy()
+    	end
+})
+-- universal
+local UTab = Window:CreateTab({
+	Name = "Universal",
 	Icon = "menu",
 	ImageSource = "Material",
 	ShowTitle = true
 })
-local DestroyButton = MainTab:CreateButton({
-	Name = "destroy gui",
+local u1 = UTab:CreateButton({
+	Name = "infinite yield",
 	Description = nil,
     	Callback = function()
-            Luna:Destroy()
+    	    loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
     	end
 })
-
+local u2 = UTab:CreateButton({
+	Name = "show desync",
+	Description = nil,
+    	Callback = function()
+    	    loadstring(game:HttpGet("https://raw.githubusercontent.com/Royynn/nhub/refs/heads/main/desync.lua"))()
+    	end
+})
+local u3 = UTab:CreateButton({
+	Name = "friend 3sp",
+	Description = nil,
+    	Callback = function()
+    	    loadstring(game:HttpGet("https://raw.githubusercontent.com/Royynn/nhub/refs/heads/main/friend3sp.lua"))()
+    	end
+})
+local ANIM_ID = 136720812089001
+local LOOPED = true
+local PLAYBACK_SPEED = 1
+local ANIM_WEIGHT = 99
+local player = Players.LocalPlayer
+local function loadAnimation(char)
+    local humanoid = char:WaitForChild("Humanoid")
+    local animator = humanoid:FindFirstChildOfClass("Animator")
+    if not animator then
+        animator = Instance.new("Animator")
+        animator.Parent = humanoid
+    end
+    local anim = Instance.new("Animation")
+    anim.AnimationId = "rbxassetid://" .. ANIM_ID
+    return animator:LoadAnimation(anim)
+end
+local currentTrack
+local u4 = UTab:CreateToggle({
+	Name = "shake ass",
+	Description = nil,
+	CurrentValue = false,
+    	Callback = function(Value)
+            local char = player.Character or player.CharacterAdded:Wait()
+            if Value then
+                currentTrack = loadAnimation(char)
+                currentTrack.Looped = LOOPED
+                currentTrack.Priority = Enum.AnimationPriority.Action4
+                currentTrack:Play(0, ANIM_WEIGHT)
+                currentTrack:AdjustSpeed(PLAYBACK_SPEED)
+            else
+                currentTrack:Stop(0)
+                currentTrack:Destroy()
+                currentTrack = nil
+            end
+    	end
+}, "Toggle")
+-- chat
+local ChatTab = Window:CreateTab({
+	Name = "edate chat with niggers",
+	Icon = "chat",
+	ImageSource = "Material",
+	ShowTitle = true
+})
+local c1 = ChatTab:CreateButton({
+	Name = "cat bypasser",
+	Description = nil,
+    	Callback = function()
+    	    loadstring(game:HttpGet("https://raw.githubusercontent.com/shadow62x/catbypass/main/upfix"))()
+    	end
+})
+local c2 = ChatTab:CreateButton({
+	Name = "clear chat",
+	Description = nil,
+    	Callback = function()
+    	    for _ = 1, 4 do
+                task.wait(0.1)
+                game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync(string.rep("　", 66))
+            end
+    	end
+})
+ChatTab:CreateSection('-- set language to "Қазақ тілі" for this to work')
+local c3 = ChatTab:CreateButton({
+	Name = "nigger spam",
+	Description = nil,
+    	Callback = function()
+    	    task.wait(0.1)
+            game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync(string.rep("　", 66))
+            task.wait(0.1)
+            game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("" .. string.rep(" ", 100) .. "fมϲҟถเ่ʛʛeꞅธ")
+            task.wait(0.1)
+            game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("" .. string.rep(" ", 100) .. "เ่ꜧสteถเ่ʛʛeꞅธ")
+            task.wait(0.1)
+            game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("" .. string.rep(" ", 100) .. "kῘllสllⴖῘʛʛeꞅṡַ")
+            task.wait(0.1)
+            game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("" .. string.rep(" ", 100) .. "cมtสถiggสรხꞅสiถiถһสꙆf")
+    	end
+})
+local c4 = ChatTab:CreateButton({
+	Name = "rape niggas",
+	Description = nil,
+    	Callback = function()
+    	    task.wait(0.1)
+            game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("һสveถ'tมรedtһewჿꞅd'ꞅสpe'iถสwһiꙆe...")
+            task.wait(1)
+            game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("iꞅสpedꙆiꝁe6ჿꞅ7ถiggeꞅรtჿdสy")
+    	end
+})
+local c5 = ChatTab:CreateButton({
+	Name = "shut up nigger",
+	Description = nil,
+    	Callback = function()
+    	    task.wait(0.1)
+            game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("ṡַḥַụַṭַụṗַⴖῘʛʛeꞅ")
+    	end
+})
+local c6 = ChatTab:CreateButton({
+	Name = "ez nigga",
+	Description = nil,
+    	Callback = function()
+    	    task.wait(0.1)
+            game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("ezⴖเ่ʛʛส")
+    	end
+})
+local c7 = ChatTab:CreateButton({
+	Name = "im jerking",
+	Description = nil,
+    	Callback = function()
+    	    task.wait(0.1)
+            game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync("іṁjәַꞅꝁịַṅַg")
+    	end
+})
 -- ink game
 local inkgameid = {
     [99567941238278] = true,
@@ -59,60 +222,7 @@ if inkgameid[game.PlaceId] then
 	Name = "guard 3sp",
 	Description = nil,
     	Callback = function()
-            local espTransparency = 0.5
-            function getRoot(char)
-                local rootPart = char:FindFirstChild('HumanoidRootPart') or char:FindFirstChild('Torso') or char:FindFirstChild('UpperTorso')
-                return rootPart
-            end
-            function CHMS(character, espColor)
-                task.spawn(function()
-                    wait()
-                    repeat task.wait(0.5) until character and getRoot(character) and character:FindFirstChildOfClass("Humanoid")
-                        local ESPholder = Instance.new("Folder")
-                        ESPholder.Name = character.Name..'_CHMS'
-                        ESPholder.Parent = COREGUI
-                        for _, part in pairs(character:GetChildren()) do
-                            if part:IsA("BasePart") then
-                                local adorn = Instance.new("BoxHandleAdornment")
-                                adorn.Name = character.Name
-                                adorn.Parent = ESPholder
-                                adorn.Adornee = part
-                                adorn.AlwaysOnTop = true
-                                adorn.ZIndex = 10
-                                adorn.Size = part.Size
-                                adorn.Transparency = espTransparency
-                                adorn.Color3 = espColor or Color3.new(1, 0, 0)
-                            end
-                        end
-                        character.Humanoid.Died:Connect(function()
-                            ESPholder:Destroy()
-                        end)
-                        ESPholder.AncestryChanged:Connect(function(_, parent)
-                            if not parent then
-                                ESPholder:Destroy()
-                            end
-                        end)
-                end)
-            end
-            function checkifguard(child)
-                if child:WaitForChild("TypeOfGuard", 3) then
-                    return true
-                end
-                return false
-            end
-            workspace.Live.ChildAdded:Connect(function(ch)
-                if checkifguard(ch) then
-                    CHMS(ch, Color3.new(1, 0, 0))
-                end
-            end)
-            for _, ch in pairs(workspace.Live:GetChildren()) do
-                if checkifguard(ch) then
-                    CHMS(ch, Color3.new(1, 0, 0))
-                end
-            end
-    	end
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/Royynn/nhub/refs/heads/main/guard3sp.lua"))()
+        end
     })
 end
-game.Players.LocalPlayer.OnTeleport:Connect(function(State)
-    queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/Royynn/nhub/refs/heads/main/nhub.lua"))()')
-end)
