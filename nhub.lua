086d5df8ff1671e78dc4d7e9a40cad9606676cdc
Roynn <55062240+Royynn@users.noreply.github.com>
2025-09-11 16:22:45 +0300

    Update nhub.lua

--- nhub.lua
+++ nhub.lua
@@ -1,5 +1,6 @@
 local Players = game:GetService("Players")
 local TeleportService = game:GetService("TeleportService")
+local HttpService = game:GetService("HttpService")
 PlaceId, JobId = game.PlaceId, game.JobId
 
 cc = game.Players.LocalPlayer.OnTeleport:Connect(function(State)
@@ -29,6 +30,29 @@ local m1 = MainTab:CreateButton({
                 TeleportService:Teleport(PlaceId, Players.LocalPlayer)
             else
                 TeleportService:TeleportToPlaceInstance(PlaceId, JobId, Players.LocalPlayer)
+            end
+    	end
+})
+local m2 = MainTab:CreateButton({
+	Name = "serverhop",
+	Description = nil,
+    	Callback = function()
+    	    local servers = {}
+            local req = game:HttpGet("https://games.roblox.com/v1/games/" .. PlaceId .. "/servers/Public?sortOrder=Desc&limit=100&excludeFullGames=true")
+            local body = HttpService:JSONDecode(req)
+
+            if body and body.data then
+                for i, v in next, body.data do
+                    if type(v) == "table" and tonumber(v.playing) and tonumber(v.maxPlayers) and v.playing < v.maxPlayers and v.id ~= JobId then
+                        table.insert(servers, 1, v.id)
+                    end
+                end
+            end
+
+            if #servers > 0 then
+                TeleportService:TeleportToPlaceInstance(PlaceId, servers[math.random(1, #servers)], Players.LocalPlayer)
+            else
+                print("Couldn't find a server.")
             end
     	end
 })
